61c1704efc6ad87a14f31c822ab96e0a6559db6c
Andrea <massironi.andrea@gmail.com>
2015-01-23 15:11:56 +0100

    up

--- test/crab/README.md
+++ test/crab/README.md
@@ -37,7 +37,10 @@ Samples
     /DYJetsToLL_M-50_13TeV-madgraph-pythia8/Spring14dr-PU_S14_POSTLS170_V6-v1/AODSIM
     /DYToEE_M_50_628_EGamma_13TeV/Summer12-PU_S14_DESIGN70_V7-v1/AODSIM
     /DYToEE_M-50_Tune4C_13TeV-pythia8/Phys14DR-PU40bx25_tsg_castor_PHYS14_25_V1-v2/AODSIM
-
+    /DoubleElectron/Run2012D-PromptReco-v1/AOD
+    /DoubleElectron/Run2012D-22Jan2013-v1/AOD
+    /DoubleElectron/Run2012C-24Aug2012-v1/AOD
+    /SingleElectron/Run2012A-PromptReco-v1/AOD
     
 
 
--- test/crab/crab_Dump_DATA.py
+++ test/crab/crab_Dump_DATA.py
@@ -3,7 +3,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 config.section_('General')
 config.General.transferOutputs = True
-config.General.requestName = 'DATA'
+config.General.requestName = 'DATAsingleEle'
 
 config.section_('JobType')
 config.JobType.psetName = '../RAW2RECO_DATA_cfg.py'
@@ -12,13 +12,14 @@ config.JobType.outputFiles = ['treeECALAlignment.root']
 config.JobType.pyCfgParams = ['inputFiles=FAKEINPUT', 'outputFile=treeECALAlignment.root']
 
 config.section_('Data')
-config.Data.inputDataset = '/DoubleElectron/Run2012C-24Aug2012-v1/AOD'
+#config.Data.inputDataset = '/DoubleElectron/Run2012C-24Aug2012-v1/AOD'
+config.Data.inputDataset = '/SingleElectron/Run2012A-PromptReco-v1/AOD'
 config.Data.unitsPerJob = 10   # since files based, 10 files per job
 config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
 #config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
 config.Data.splitting = 'LumiBased'    # FileBased
 config.Data.useParent = True           # Important!
-config.Data.runRange = '198023-198116'
+config.Data.runRange = '193650-193686'
 
 config.Data.outLFN = '/store/user/amassiro/ECAL/Alignment/test21Jan2015'
 
