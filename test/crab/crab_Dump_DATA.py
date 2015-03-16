467f09499e18b0e4680a955bca9a303c1edb5c34
Andrea <massironi.andrea@gmail.com>
2015-03-16 11:08:38 +0100

    up

--- test/crab/crab_Dump_DATA.py
+++ test/crab/crab_Dump_DATA.py
@@ -13,8 +13,8 @@ config.JobType.outputFiles = ['treeECALAlignment.root']
 config.JobType.pyCfgParams = ['inputFiles=FAKEINPUT', 'outputFile=treeECALAlignment.root']
 
 config.section_('Data')
-#config.Data.inputDataset = '/DoubleElectron/Run2012C-24Aug2012-v1/AOD'
-config.Data.inputDataset = '/DoubleElectron/Run2012D-22Jan2013-v1/AOD'
+config.Data.inputDataset = '/DoubleElectron/Run2012C-24Aug2012-v1/AOD'
+#config.Data.inputDataset = '/DoubleElectron/Run2012D-22Jan2013-v1/AOD'
 config.Data.unitsPerJob = 10   # since files based, 10 files per job
 config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
 #config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
@@ -22,7 +22,7 @@ config.Data.splitting = 'LumiBased'    # FileBased
 config.Data.useParent = True           # Important!
 #config.Data.runRange = '193650-193686'
 
-config.Data.outLFN = '/store/user/amassiro/ECAL/Alignment/test27Jan2015'
+config.Data.outLFN = '/store/user/amassiro/ECAL/Alignment/test16Mar2015'
 
 config.section_('Site')
 config.Site.storageSite = 'T2_CH_CERN'
