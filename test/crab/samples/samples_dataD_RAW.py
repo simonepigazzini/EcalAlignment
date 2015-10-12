e96facb63ad2ecb6b6a3991e1cf5d18831e1ded1
Andrea <massironi.andrea@gmail.com>
2015-10-12 11:46:21 +0200

    run D

---
+++ test/crab/samples/samples_dataD_RAW.py
@@ -0,0 +1,34 @@
+########
+# Data
+########
+
+#        CRAB task name          DAS name                                 
+samples['SingleElectron']   = ['/SingleElectron/Run2015D-PromptReco-v3/AOD',  ['outputFile=treeECALAlignment.root']]
+
+ 
+
+
+########
+# Alternative global configuration
+########
+
+config.Data.splitting = 'LumiBased'    # FileBased
+config.Data.unitsPerJob   = 10
+# config.Data.runRange = '251562-254790'
+
+# 74X_dataRun2_Candidate_2015_10_09_09_41_36
+
+
+#config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/amassiro/ECALAlignment/DATA29Aug2015AODAllData'
+#config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-254879_13TeV_PromptReco_Collisions15_JSON.txt"
+#config.General.workArea     = 'crab_projects_27Aug_PromptTrkAlign'
+
+
+
+config.JobType.psetName = '../reco_RAW2DIGI_RECO_AOD.py'
+config.JobType.maxMemoryMB = 2500    # 2.5 GB
+
+config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/amassiro/ECALAlignment/DATA12Oct2015AODAllDataRunDRAW'
+config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-257599_13TeV_PromptReco_Collisions15_25ns_JSON.txt"
+config.General.workArea     = 'crab_projects_12Oct_PromptTrkAlign_RunD_RAW'
+config.Data.allowNonValidInputDataset = True
