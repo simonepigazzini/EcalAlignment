ba2f2ea0b3ec5d0057f19eec38f288e5050d08d7
amassiro <massironi.andrea@gmail.com>
2016-05-08 20:52:57 +0200

    up

--- test/crab/samples/samples_spring16.py
+++ test/crab/samples/samples_spring16.py
@@ -4,7 +4,7 @@
 #        CRAB task name          DAS name                                                                                                              
 samples['25ns_DYJetsToEE']   = ['/DYToEE_NNPDF30_13TeV-powheg-pythia8/RunIISpring16MiniAODv1-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/MINIAODSIM',  ['outputFile=treeECALAlignment.root']]
 
-# hadd /tmp/amassiro/ECAL_DYMC.root /tmp/amassiro/eos/cms/store/group/dpg_ecal/alca_ecalcalib/amassiro/ECALAlignment/MC15Dec2015/DYToEE_NNPDF30_13TeV-powheg-pythia8/crab_25ns_DYJetsToEE/151215_105021/0000/tr*.root
+# hadd /tmp/amassiro/ECAL_DYMC.root /tmp/amassiro/eos/cms/store/group/dpg_ecal/alca_ecalcalib/amassiro/ECALAlignment/MC07May2016/DYToEE_NNPDF30_13TeV-powheg-pythia8/crab_25ns_DYJetsToEE/160507_135010/0000/tr*.root
 
 
 
