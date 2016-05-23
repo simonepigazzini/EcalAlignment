af4c4e9d92fdd994e4a4f56a39bcd8cc4045a2fb
amassiro <massironi.andrea@gmail.com>
2016-05-23 15:33:10 +0200

    up

--- test/crab/samples/samples_spring16.py
+++ test/crab/samples/samples_spring16.py
@@ -6,10 +6,12 @@
 samples['25ns_WJetsToENu']   = ['/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv1-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/MINIAODSIM',  ['outputFile=treeECALAlignment.root']]
 
 
+#
 # hadd /tmp/amassiro/ECAL_DYMC.root /tmp/amassiro/eos/cms/store/group/dpg_ecal/alca_ecalcalib/amassiro/ECALAlignment/MC07May2016/DYToEE_NNPDF30_13TeV-powheg-pythia8/crab_25ns_DYJetsToEE/160507_135010/0000/tr*.root
 #  /tmp/amassiro/eos/cms/store/group/dpg_ecal/alca_ecalcalib/amassiro/ECALAlignment/MC07May2016/DYToEE_NNPDF30_13TeV-powheg-pythia8/ECAL_DYMC.root 
-# hadd /tmp/amassiro/ECAL_WevMC.root /tmp/amassiro/eos/cms/store/group/dpg_ecal/alca_ecalcalib/amassiro/ECALAlignment/MC07May2016/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_WJetsToENu/160521_134445/*/tr*.root
-
+# hadd /tmp/amassiro/ECAL_WevMC.root /tmp/amassiro/eos/cms/store/group/dpg_ecal/alca_ecalcalib/amassiro/ECALAlignment/MC07May2016/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_WJetsToENu/160523_082237/*/tr*.root
+#  /tmp/amassiro/eos/cms/store/group/dpg_ecal/alca_ecalcalib/amassiro/ECALAlignment/MC07May2016/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_25ns_WJetsToENu/ECAL_WevMC.root
+#
 
 
 ########
