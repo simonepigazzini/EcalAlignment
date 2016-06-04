2de98ff88c076eeb480a01096faa2db8534437c7
amassiro <massironi.andrea@gmail.com>
2016-06-04 17:02:53 +0200

    new

--- bin/drawSingleModule.cpp
+++ bin/drawSingleModule.cpp
@@ -2,6 +2,9 @@
 
 #include "../test/plot/tools/Functions.h"
 
+#include "../test/plot/tools/CMS_lumi.C"
+
+
 #include "TH1F.h"
 #include "TH2F.h"
 #include "TTree.h"
@@ -23,7 +26,9 @@
 
 
 
-void drawSingleModule(TChain* InFile, TChain* InFileComparison , TString nameOutputDir, TString commonCut = "1", int iEB=-100, int iEE=-100, int isMC=1, int specialRegions = 0, int specialZeroTesla = 0){
+
+
+void drawSingleModule(TChain* InFile, TChain* InFileComparison , TString nameOutputDir, TString commonCut = "1", int iEB=-100, int iEE=-100, int isMC=1, int specialRegions = 0, int specialZeroTesla = 0, TChain* InFileAlternative = 0){
   
  //---- for data
  TString tempCut;
@@ -74,6 +79,16 @@ void drawSingleModule(TChain* InFile, TChain* InFileComparison , TString nameOut
  if (specialZeroTesla == 0) DEtaMC_ref = new TH1F("DEtaMC_ref"     ," MC"  ,150,-0.025,0.025);
  else                       DEtaMC_ref = new TH1F("DEtaMC_ref"     ," MC"  ,150,-0.025,0.025);
  
+
+ TH1F* DPhiMC_alt;
+ if (specialZeroTesla == 0) DPhiMC_alt = new TH1F("DPhiMC_alt"     ," MC"  ,200,-0.02,0.02);
+ else                       DPhiMC_alt = new TH1F("DPhiMC_alt"     ," MC"  ,150,-0.025,0.025);
+ TH1F* DPhiMC_alt_ep  = new TH1F("DPhiMC_alt_ep",  " MC"  ,200,-0.02,0.02);
+ TH1F* DPhiMC_alt_em  = new TH1F("DPhiMC_alt_em",  " MC"  ,200,-0.02,0.02);
+ TH1F* DEtaMC_alt;
+ if (specialZeroTesla == 0) DEtaMC_alt = new TH1F("DEtaMC_alt"     ," MC"  ,150,-0.025,0.025);
+ else                       DEtaMC_alt = new TH1F("DEtaMC_alt"     ," MC"  ,150,-0.025,0.025);
+ 
  
  ///---- text info (begin) ----
  TLatex*  tinfoDPhi    = new TLatex(0.2,0.8,"");
@@ -85,25 +100,48 @@ void drawSingleModule(TChain* InFile, TChain* InFileComparison , TString nameOut
  TLatex*  tinfoDEta_ref    = new TLatex(0.2,0.7,"");
  TLatex*  tinfoDPhi_ref_ep = new TLatex(0.2,0.7,"");
  TLatex*  tinfoDPhi_ref_em = new TLatex(0.2,0.7,"");
+
+ TLatex*  tinfoDPhi_alt    = new TLatex(0.2,0.7,"");
+ TLatex*  tinfoDEta_alt    = new TLatex(0.2,0.7,"");
+ TLatex*  tinfoDPhi_alt_ep = new TLatex(0.2,0.7,"");
+ TLatex*  tinfoDPhi_alt_em = new TLatex(0.2,0.7,"");
  ///---- text info (end) ----
+ 
+ 
+ float ScaleAxis = 1.5;
  
  createHisto(tinfoDPhi, DPhiMC, InFile, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut.Data());
  createHisto(tinfoDEta, DEtaMC, InFile, "deltaEtaSuperClusterAtVtx", "#Delta#eta",CompleteCut.Data());
+ 
+ createHisto(tinfoDPhi_ref, DPhiMC_ref, InFileComparison, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut.Data(), 1);
+ createHisto(tinfoDEta_ref, DEtaMC_ref, InFileComparison, "deltaEtaSuperClusterAtVtx", "#Delta#eta",CompleteCut.Data(), 1);
  
  TString CompleteCut_ep = Form ("(%s) && (eleCharge>0)",CompleteCut.Data());
  TString CompleteCut_em = Form ("(%s) && (eleCharge<0)",CompleteCut.Data());
  
  createHisto(tinfoDPhi_ep, DPhiMC_ep, InFile, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut_ep.Data());
  createHisto(tinfoDPhi_em, DPhiMC_em, InFile, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut_em.Data());
- 
- createHisto(tinfoDPhi_ref, DPhiMC_ref, InFileComparison, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut.Data(), 1);
- createHisto(tinfoDEta_ref, DEtaMC_ref, InFileComparison, "deltaEtaSuperClusterAtVtx", "#Delta#eta",CompleteCut.Data(), 1);
- 
+  
  createHisto(tinfoDPhi_ref_ep, DPhiMC_ref_ep, InFileComparison, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut_ep.Data(), 1);
  createHisto(tinfoDPhi_ref_em, DPhiMC_ref_em, InFileComparison, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut_em.Data(), 1);
   
+ float min_legend = 0.62;
+ 
+ if (InFileAlternative != 0) {
+//    ScaleAxis = 2.5;
+//    min_legend = 0.65;
+   ScaleAxis = 3.7;
+   min_legend = 0.54;
+   
+   createHisto(tinfoDPhi_alt, DPhiMC_alt, InFileAlternative, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut.Data(), 2);
+   createHisto(tinfoDEta_alt, DEtaMC_alt, InFileAlternative, "deltaEtaSuperClusterAtVtx", "#Delta#eta",CompleteCut.Data(), 2);
+ 
+   createHisto(tinfoDPhi_alt_ep, DPhiMC_alt_ep, InFileAlternative, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut_ep.Data(), 2);
+   createHisto(tinfoDPhi_alt_em, DPhiMC_alt_em, InFileAlternative, "deltaPhiSuperClusterAtVtx", "#Delta#phi",CompleteCut_em.Data(), 2);
+ }
+ 
  ///---- legend (begin) ----
- TLegend* legend = new TLegend(0.62,0.62,0.88,0.83);
+ TLegend* legend = new TLegend(0.62,min_legend,0.88,min_legend+(0.83-0.65));
  if (isMC) {
   legend->AddEntry(DEtaMC,"MC","f");
  }
@@ -113,16 +151,19 @@ void drawSingleModule(TChain* InFile, TChain* InFileComparison , TString nameOut
  legend->SetFillColor(kWhite);
 
  legend->AddEntry(DEtaMC_ref,"MC","f");
- 
+
+ if (InFileAlternative != 0) {
+   legend->AddEntry(DEtaMC_alt,"DATA old","f");
+ }
  
  ///---- legend (end) ----
  
  
  ///---- text (begin) ----
- TLatex *tEB = new TLatex(0.2,0.7,"#splitline{EB}{#splitline{CMS 2011}{L= 4.98 fb^{-1}}}");
- TLatex *tEE = new TLatex(0.2,0.7,"#splitline{EE}{#splitline{CMS 2011}{L= 4.98 fb^{-1}}}");
- tEB->SetNDC(true);
- tEE->SetNDC(true);
+//  TLatex *tEB = new TLatex(0.2,0.7,"#splitline{EB}{#splitline{CMS 2011}{L= 4.98 fb^{-1}}}");
+//  TLatex *tEE = new TLatex(0.2,0.7,"#splitline{EE}{#splitline{CMS 2011}{L= 4.98 fb^{-1}}}");
+//  tEB->SetNDC(true);
+//  tEE->SetNDC(true);
  ///---- text (end) ----
  
  TString toDoShell;
@@ -154,15 +195,18 @@ void drawSingleModule(TChain* InFile, TChain* InFileComparison , TString nameOut
  
  TCanvas* cDPhi = new TCanvas("cDphi","cDphi",700,700);
  DPhiMC->Draw("PE");
- DPhiMC->GetYaxis()->SetRangeUser( 0, 1.5 * DPhiMC->GetMaximum() );
+ DPhiMC->GetYaxis()->SetRangeUser( 0, ScaleAxis *  DPhiMC->GetMaximum() );
  float integral = DPhiMC->Integral();
  DPhiMC_ref->Scale(integral / DPhiMC_ref->Integral());
  DPhiMC_ref->Draw("same");
  DPhiMC->Draw("PE same");
+ if (InFileAlternative != 0) { DPhiMC_alt->Scale(integral / DPhiMC_alt->Integral());  DPhiMC_alt->Draw("PE same");  tinfoDPhi_alt->Draw(); }
  tinfoDPhi_ref->Draw();
  tinfoDPhi->Draw();
  gPad->SetGrid();
  toDoShell = Form("%s/images/cDphi_%d_%d_%d.png",nameOutputDir.Data(), iEB, iEE, specialRegions);
+ gPad->SaveAs(toDoShell.Data());
+ toDoShell = Form("%s/images/cDphi_%d_%d_%d.C",nameOutputDir.Data(), iEB, iEE, specialRegions);
  gPad->SaveAs(toDoShell.Data());
  myfile << DPhiMC->GetMean() << " " << DPhiMC->GetRMS() << " " << DPhiMC->GetEntries() << "      ";
 
@@ -172,49 +216,64 @@ void drawSingleModule(TChain* InFile, TChain* InFileComparison , TString nameOut
  
  TCanvas* cDPhi_ep = new TCanvas("cDPhi_ep","cDPhi_ep",700,700);
  DPhiMC_ep->Draw("PE");
- DPhiMC_ep->GetYaxis()->SetRangeUser( 0, 1.5 * DPhiMC_ep->GetMaximum() );
+ DPhiMC_ep->GetYaxis()->SetRangeUser( 0, ScaleAxis *  DPhiMC_ep->GetMaximum() );
  integral = DPhiMC_ep->Integral();
  DPhiMC_ref_ep->Scale(integral / DPhiMC_ref_ep->Integral());
  DPhiMC_ref_ep->Draw("same");
  DPhiMC_ep->Draw("PE same");
+ if (InFileAlternative != 0) { DPhiMC_alt_ep->Scale(integral / DPhiMC_alt_ep->Integral());  DPhiMC_alt_ep->Draw("PE same"); tinfoDPhi_alt_ep->Draw();  }
  tinfoDPhi_ref_ep->Draw();
  tinfoDPhi_ep->Draw();
+ CMS_lumi( cDPhi_ep, 4, 10 );
  gPad->SetGrid();
  toDoShell = Form("%s/images/cDPhi_ep_%d_%d_%d.png",nameOutputDir.Data(), iEB, iEE, specialRegions);
+ gPad->SaveAs(toDoShell.Data());
+ toDoShell = Form("%s/images/cDPhi_ep_%d_%d_%d.C",nameOutputDir.Data(), iEB, iEE, specialRegions);
  gPad->SaveAs(toDoShell.Data());
  myfile << DPhiMC_ep->GetMean() << " " << DPhiMC_ep->GetRMS() << " " << DPhiMC_ep->GetEntries() << "      ";
  
  TCanvas* cDPhi_em = new TCanvas("cDPhi_em","cDPhi_em",700,700);
  DPhiMC_em->Draw("PE");
- DPhiMC_em->GetYaxis()->SetRangeUser( 0, 1.5 * DPhiMC_em->GetMaximum() );
+ DPhiMC_em->GetYaxis()->SetRangeUser( 0, ScaleAxis *  DPhiMC_em->GetMaximum() );
  integral = DPhiMC_em->Integral();
  DPhiMC_ref_em->Scale(integral / DPhiMC_ref_em->Integral());
  DPhiMC_ref_em->Draw("same");
  DPhiMC_em->Draw("PE same");
+ if (InFileAlternative != 0) { DPhiMC_alt_em->Scale(integral / DPhiMC_alt_em->Integral()); DPhiMC_alt_em->Draw("PE same");  tinfoDPhi_alt_em->Draw();  }
  tinfoDPhi_ref_em->Draw();
  tinfoDPhi_em->Draw();
+ CMS_lumi( cDPhi_em, 4, 10 );
  gPad->SetGrid();
  toDoShell = Form("%s/images/cDPhi_em_%d_%d_%d.png",nameOutputDir.Data(), iEB, iEE, specialRegions);
+ gPad->SaveAs(toDoShell.Data());
+ toDoShell = Form("%s/images/cDPhi_em_%d_%d_%d.C",nameOutputDir.Data(), iEB, iEE, specialRegions);
  gPad->SaveAs(toDoShell.Data());
  myfile << DPhiMC_em->GetMean() << " " << DPhiMC_em->GetRMS() << " " << DPhiMC_em->GetEntries() << "      ";
  
  
  
  legend->Draw();
+ // writing the lumi information and the CMS "logo"
+ CMS_lumi( cDPhi, 4, 10 );
+ 
  cDPhi->SetGrid(); gPad->Update();
  
  TCanvas* cDEta = new TCanvas("cDeta","cDeta",700,700);
  DEtaMC->Draw("PE");
- DEtaMC->GetYaxis()->SetRangeUser( 0, 1.5 * DEtaMC->GetMaximum() );
+ DEtaMC->GetYaxis()->SetRangeUser( 0, ScaleAxis *  DEtaMC->GetMaximum() );
  integral = DEtaMC->Integral();
  DEtaMC_ref->Scale(integral / DEtaMC_ref->Integral());
  DEtaMC_ref->Draw("same");
  DEtaMC->Draw("PE same");
+ if (InFileAlternative != 0) {  DEtaMC_alt->Scale(integral / DEtaMC_alt->Integral()); DEtaMC_alt->Draw("PE same");  tinfoDEta_alt->Draw(); }
  tinfoDEta_ref->Draw();
  tinfoDEta->Draw();
  legend->Draw();
  cDEta->SetGrid(); gPad->Update();
+ CMS_lumi( cDEta, 4, 10 );
  toDoShell = Form("%s/images/cDEta_%d_%d_%d.png",nameOutputDir.Data(), iEB, iEE, specialRegions);
+ gPad->SaveAs(toDoShell.Data());
+ toDoShell = Form("%s/images/cDEta_%d_%d_%d.C",nameOutputDir.Data(), iEB, iEE, specialRegions);
  gPad->SaveAs(toDoShell.Data());
  myfile << DEtaMC->GetMean() << " " << DEtaMC->GetRMS() << " " << DEtaMC->GetEntries() << "      ";
  
@@ -239,8 +298,8 @@ void drawSingleModule(TChain* InFile, TChain* InFileComparison , TString nameOut
  delete cDPhi_ep;
  delete cDEta;
  
- delete tEB;
- delete tEE;
+//  delete tEB;
+//  delete tEE;
  
 }
 
@@ -258,14 +317,21 @@ int main(int argc, char** argv) {
   std::string nameInFileRoot   = subPSetInput.getParameter<std::string> ("nameInFileRoot") ;
   std::string nameInFileRootComparison   = subPSetInput.getParameter<std::string> ("nameInFileRootComparison") ;
   std::string commonCut = subPSetInput.getParameter<std::string> ("commonCut") ;
-  
   std::string nameOutputDir = subPSetInput.getParameter<std::string> ("nameOutputDir") ;
   
+  std::string treeName = subPSetInput.getUntrackedParameter<std::string> ("treeName","ntupleEcalAlignment/myTree") ;
+  
   //---- special setup
-  int specialZeroTesla = subPSetInput.getUntrackedParameter<int> ("specialZeroTesla") ;
+  int specialZeroTesla = subPSetInput.getUntrackedParameter<int> ("specialZeroTesla", 0) ;
+  
+  //---- if you want to compare 2 sets of data
+  std::string nameInFileRootAlternative = subPSetInput.getUntrackedParameter<std::string> ("nameInFileRootAlternative", "NONE") ;
   
   
   
+  
+  
+  std::cout << " treeName =                 " << treeName << std::endl;  
   std::cout << " nameInFileRoot =           " << nameInFileRoot << std::endl;
   std::cout << " nameInFileRootComparison = " << nameInFileRootComparison << std::endl;
   std::cout << " commonCut =                " << commonCut << std::endl;
@@ -273,11 +339,17 @@ int main(int argc, char** argv) {
   std::cout << " specialZeroTesla =         " << specialZeroTesla << std::endl;
   
   
-  TChain* InFile   = new TChain("ntupleEcalAlignment/myTree");
+  if (nameInFileRootAlternative != "NONE")   std::cout << " nameInFileRootAlternative = " << nameInFileRootAlternative << std::endl;
+  
+  TChain* InFile   = new TChain(treeName.c_str());
   InFile->Add(nameInFileRoot.c_str());
   
-  TChain* InFileComparison   = new TChain("ntupleEcalAlignment/myTree");
+  TChain* InFileComparison   = new TChain(treeName.c_str());
   InFileComparison->Add(nameInFileRootComparison.c_str());
+  
+  
+  TChain* InFileAlternative   = new TChain(treeName.c_str());
+  if (nameInFileRootAlternative != "NONE") InFileAlternative->Add(nameInFileRootAlternative.c_str());
   
   
   //---- prepare style
@@ -305,6 +377,15 @@ int main(int argc, char** argv) {
   TEntryList *myListMC = (TEntryList*) gDirectory->Get("myListMC");
   InFileComparison->SetEntryList(myListMC); 
   
+  
+  if (nameInFileRootAlternative != "NONE") {
+    InFileAlternative->SetEntryList(0); 
+    InFileAlternative->Draw(">> myListAlternative",tempCut_data.Data(),"entrylist");
+    TEntryList *myListAlternative = (TEntryList*) gDirectory->Get("myListAlternative");
+    InFileAlternative->SetEntryList(myListAlternative); 
+  }
+  
+  
   //---- create directories
   TString toDoShell;
   toDoShell = Form("mkdir %s ;",nameOutputDir.c_str());
@@ -317,21 +398,42 @@ int main(int argc, char** argv) {
   system(toDoShell.Data());
   
 
+  std::cout << " first the special regions ..." << std::endl;
+  
   //---- summary regions
   for (int iSpecial = 0; iSpecial <4; iSpecial++) {
-    drawSingleModule(InFile, InFileComparison, nameOutputDir, commonCut, -100, -100, 0, iSpecial+1, 0);
+    if (nameInFileRootAlternative != "NONE") {
+      drawSingleModule(InFile, InFileComparison, nameOutputDir, commonCut, -100, -100, 0, iSpecial+1, 0, InFileAlternative);
+    }
+    else {
+      drawSingleModule(InFile, InFileComparison, nameOutputDir, commonCut, -100, -100, 0, iSpecial+1, 0);      
+    }
   }
   
+  std::cout << " then the EndCap ..." << std::endl;
   //---- EE
   for (int iEE = 0; iEE <4; iEE++) {
-    drawSingleModule(InFile, InFileComparison, nameOutputDir, commonCut, -100, iEE, 0, 0, 0);
-  }
-    
-  //---- EB
-  for (int iEB = 0; iEB <36; iEB++) {
-    drawSingleModule(InFile, InFileComparison, nameOutputDir, commonCut, iEB, -100, 0, 0, 0);
+    if (nameInFileRootAlternative != "NONE") {
+      drawSingleModule(InFile, InFileComparison, nameOutputDir, commonCut, -100, iEE, 0, 0, 0, InFileAlternative);
+    }
+    else {
+      drawSingleModule(InFile, InFileComparison, nameOutputDir, commonCut, -100, iEE, 0, 0, 0);
+    }
   }
 
+  std::cout << " and now the Barrel ..." << std::endl;
+  //---- EB
+  for (int iEB = 0; iEB <36; iEB++) {
+    if (nameInFileRootAlternative != "NONE") {
+      drawSingleModule(InFile, InFileComparison, nameOutputDir, commonCut, iEB, -100, 0, 0, 0, InFileAlternative);
+    }
+    else {
+      drawSingleModule(InFile, InFileComparison, nameOutputDir, commonCut, iEB, -100, 0, 0, 0);
+    }
+  }
+
+  std::cout << " Done." << std::endl;
+  
 }
 
 
