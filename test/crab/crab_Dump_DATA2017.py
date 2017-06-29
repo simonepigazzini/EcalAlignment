8973c8b9b9a14ca316246a4e28ae90c76e81f1bc
wamorkart <wamorkar.t@husky.neu.edu>
2017-06-29 11:08:51 -0400

    Update crab_Dump_DATA2017.py

--- test/crab/crab_Dump_DATA2017.py
+++ test/crab/crab_Dump_DATA2017.py
@@ -5,7 +5,7 @@ config.section_('General')
 config.General.transferOutputs = True
 #config.General.requestName = 'DATAsingleEle'
 #config.General.requestName = 'DATAdoubleEle22Jun_RAWRECO_v4'
-config.General.requestName = 'DATAdoubleEle23Jun_RAWRECO_runA'
+config.General.requestName = 'DATAdoubleEle28Jun_RAWRECO_RunA_v3_newdata'
 config.section_('JobType')
 #config.JobType.psetName = '../RAW2RECO_DATA_cfg.py'
 #config.JobType.psetName = '../Dump_DATA_cfg.py'
@@ -19,15 +19,16 @@ config.section_('Data')
 #config.Data.inputDataset = '/DoubleElectron/Run2012D-22Jan2013-v1/AOD'
 #-------2017---------
 #config.Data.inputDataset = '/DoubleEG/Run2017A-PromptReco-v3/MINIAOD'
-config.Data.inputDataset = '/DoubleEG/Run2017A-ZElectron-PromptReco-v2/RAW-RECO'
+config.Data.inputDataset = '/DoubleEG/Run2017A-ZElectron-PromptReco-v3/RAW-RECO'
 config.Data.unitsPerJob = 10   # since files based, 10 files per job
 config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
 #config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
 config.Data.splitting = 'LumiBased'    # FileBased
-config.Data.useParent = True           # Important!
+#config.Data.useParent = True           # Important!
 #config.Data.runRange = '193650-193686'
 config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/DCSOnly/json_DCSONLY.txt'
-config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/twamorka/23Jun_Run2017A_v2_RAWRECO_parentsettrue'
+config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/twamorka/28Jun_RunA_v3_newdata'
+#config.JobType.inputFiles = ['../EBAlign_2015_jun282017.db','../EEAlign_2015_jun282017_rotationon.db']
 
 config.section_('Site')
 config.Site.storageSite = 'T2_CH_CERN'
