6ee2088c7f2deb9fb2b7824f51ef601f85f88b55
Amrutha Krishna <amruthakrishna.unni@gmail.com>
2022-09-07 15:11:54 +0200

    symmetrized MC bias values

--- test/align/AlignEB_Zee_cfg.py
+++ test/align/AlignEB_Zee_cfg.py
@@ -8,7 +8,7 @@ process.inputTree = cms.PSet(
 
   #selection = cms.string("(electrons_classification==0 && ETSC>20)"),
 
-  selection = cms.string("(electrons_classification==0 && ETSC>20) && mll<95 && mll>85  && ((abs(eta) <= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.07 && abs(SigmaIEtaIEta)<0.01) || (abs(eta) >= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.06 && abs(SigmaIEtaIEta)<0.03))"),
+  selection = cms.string("(electrons_classification==0 && ETSC>20) && mll<100 && mll>80  && ((abs(eta) <= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.07 && abs(SigmaIEtaIEta)<0.01) || (abs(eta) >= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.06 && abs(SigmaIEtaIEta)<0.03))"),
 
 #selection = cms.string("(electrons_classification==0 && ETSC>40) && mll<95 && mll>85 && etaSC < 2.1 && etaSC > -2.1 && HoE<0.3 && eleEcalIso<15 && (abs(DeltaEtaIn)<(25e-3)) && (abs(DeltaPhiIn)<(10e-3))&& ((abs(eta) <= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.07 && abs(SigmaIEtaIEta)<0.01) || (abs(eta) >= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.06 && abs(SigmaIEtaIEta)<0.03))"),
 
@@ -24,9 +24,9 @@ process.inputTree = cms.PSet(
   traslationX = cms.bool(True),
   traslationY = cms.bool(True),
   traslationZ = cms.bool(True),
-  rotationPhi   = cms.bool(False),
-  rotationTheta = cms.bool(False),
-  rotationPsi   = cms.bool(False),
+    rotationPhi   = cms.bool(False),
+    rotationTheta = cms.bool(False),
+    rotationPsi   = cms.bool(False),
 
   setRotationPhi   = cms.untracked.double(0),
   setRotationTheta = cms.untracked.double(0),
@@ -40,27 +40,27 @@ process.inputTree = cms.PSet(
 # DPhi values follow the sequence: [Positron: EB+, EE+, EB-, EE-], [Electron: EB+, EE+, EB-, EE-]
 # x = eta, y = charge
 
-    DetaBias = cms.untracked.string(" (x>0.0 && x<1.5)  * (-0.000170933)\
-                                   +(x>1.5)           * (-0.000438338)    \
-                                   +(x<0.0 && x>-1.5) * (0.000180109)    \
-                                   +(x<-1.5)          * (0.00042374) \
+    DetaBias = cms.untracked.string(" (x>0.0 && x<1.5)  * (-0.17e-3)\
+                                   +(x>1.5)           * (-0.36e-3)    \
+                                   +(x<0.0 && x>-1.5) * (0.19e-3)    \
+                                   +(x<-1.5)          * (0.26e-3) \
                                    + y*0"),
 
   DphiBias = cms.untracked.string(" (y>0)*    \
-                                  ((x>0.0 && x<1.5)   * (0.000931704)  \
-                                 + (x>1.5)            * (1.76616e-05)  \
-                                 + (x<0.0 && x>-1.5)  * (0.000265959)  \
-                                 + (x<-1.5)           * (-0.000136237)) \
+                                  ((x>0.0 && x<1.5)   * (0.95e-3)  \
+                                 + (x>1.5)            * (0.07e-3)  \
+                                 + (x<0.0 && x>-1.5)  * (0.26e-3)  \
+                                 + (x<-1.5)           * (-0.1e-5)) \
                                  + (y<0)*   \
-                                   ((x>0.0 && x<1.5)   * (-0.000184643) \
-                                 + (x>1.5)             * (0.000427552) \
-                                 + (x<0.0 && x>-1.5)  * (-0.000838635)  \
-                                 + (x<-1.5)           * (0.000408662))"),
+                                   ((x>0.0 && x<1.5)   * (-0.19e-3) \
+                                 + (x>1.5)             * (0.18e-3) \
+                                 + (x<0.0 && x>-1.5)  * (-0.85e-3)  \
+                                 + (x<-1.5)           * (0.19e-3))"),
   ## change input file appropriately
   inputFiles    = cms.vstring(
       #    'file:///eos/cms/store/group/dpg_ecal/alca_ecalcalib/twamorka/UltraLegacy_WithTrackerConditions_Conditions2/DoubleEG/crab_ZElectron-2017B-RAWReco-v1/190502_160024/0000/treeECALAlignment_499.root'
-      #'file:/eos/cms/store/group/dpg_ecal/alca_ecalcalib/automation_prompt/alignment-reco/*.root'
-     'file:///eos/cms/store/group/dpg_ecal/alca_ecalcalib/amkrishn/ECALalignment2022run3Commisioning/data/EGamma/crab_EcalAlignment_RERECO/220719_092417/0000/treeECALAlignment_run3Commissioning.root'
+      'file:///eos/cms/store/group/dpg_ecal/alca_ecalcalib/amkrishn/ECALalignment2022run3Commisioning/data/automationOutputHadd/treeECALAlignment_run3Commissioning_2022C.root'
+    #'file:///eos/cms/store/group/dpg_ecal/alca_ecalcalib/amkrishn/ECALalignment2022run3Commisioning/data/automationOutputHadd/treeECALAlignment_run3Commissioning_2022C_860pb.root'
 #'file:/afs/cern.ch/user/a/amkrishn/CMSSW_12_0_0/src/EcalValidation/EcalAlignment/test/output.root'
 
     )
@@ -68,5 +68,5 @@ process.inputTree = cms.PSet(
 
 
 process.outputTree = cms.PSet(
-   outputFile = cms.string("EBAlignmentCoefficients.txt")
+   outputFile = cms.string("EBAlignmentCoefficients")
 )
--- test/align/AlignEE_Zee_cfg.py
+++ test/align/AlignEE_Zee_cfg.py
@@ -8,7 +8,7 @@ process.inputTree = cms.PSet(
 
   #selection = cms.string("(electrons_classification==0 && ETSC>20)"),
   #selection = cms.string("(electrons_classification==0 && ETSC>30) && ((abs(eta) <= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.07 && abs(SigmaIEtaIEta)<0.01) || (abs(eta) >= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.06 && abs(SigmaIEtaIEta)<0.03))"),
-  selection = cms.string("(electrons_classification==0 && ETSC>20) && mll<95 && mll>85 && ((abs(eta) <= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.07 && abs(SigmaIEtaIEta)<0.01) || (abs(eta) >= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.06 && abs(SigmaIEtaIEta)<0.03))"),
+  selection = cms.string("(electrons_classification==0 && ETSC>20) && mll<100 && mll>80 && ((abs(eta) <= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.07 && abs(SigmaIEtaIEta)<0.01) || (abs(eta) >= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.06 && abs(SigmaIEtaIEta)<0.03))"),
 #selection = cms.string("(electrons_classification==0 && ETSC>40) && mll<95 && mll>85 && etaSC < 2.1 && etaSC > -2.1 && HoE<0.3 && eleEcalIso<15 && (abs(DeltaEtaIn)<(25e-3)) && (abs(DeltaPhiIn)<(10e-3))&& ((abs(eta) <= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.07 && abs(SigmaIEtaIEta)<0.01) || (abs(eta) >= 1.5 && (eleTrkIso + eleEcalIso + eleHcalIsoD1 + eleHcalIsoD2)/pT < 0.06 && abs(SigmaIEtaIEta)<0.03))"),
 
 #selection = cms.string("(1)"),
@@ -24,9 +24,9 @@ process.inputTree = cms.PSet(
   traslationY = cms.bool(True),
   traslationZ = cms.bool(True),
 #  rotationPhi   = cms.bool(True), # True
-  rotationPhi = cms.bool(False),
-  rotationTheta = cms.bool(False),
-  rotationPsi   = cms.bool(False), # True
+    rotationPhi = cms.bool(True),
+    rotationTheta = cms.bool(True),
+    rotationPsi   = cms.bool(True), # True
 
   setRotationPhi   = cms.untracked.double(0),
   setRotationTheta = cms.untracked.double(0),
@@ -37,22 +37,41 @@ process.inputTree = cms.PSet(
 # DEta values follow the sequence: EB+, EE+, EB-, EE-
 # DPhi values follow the sequence: [Positron: EB+, EE+, EB-, EE-], [Electron: EB+, EE+, EB-, EE-]
 # x = eta, y = charge   
-    DetaBias = cms.untracked.string(" (x>0.0 && x<1.5)  * (-0.000170933) \
-                                   +(x>1.5)           * (-0.000438338)    \
-                                   +(x<0.0 && x>-1.5) * (0.000180109)    \
-                                   +(x<-1.5)          * (0.00042374) \
+    DetaBias = cms.untracked.string(" (x>0.0 && x<1.5)  * (-0.17e-3) \
+                                   +(x>1.5)           * (-0.36e-3)    \
+                                   +(x<0.0 && x>-1.5) * (0.19e-3)    \
+                                   +(x<-1.5)          * (0.26e-3) \
                                    + y*0"),
 
   DphiBias = cms.untracked.string(" (y>0)*    \
-                                  ((x>0.0 && x<1.5)   * (0.000931704)  \
-                                 + (x>1.5)            * (1.76616e-05)  \
-                                 + (x<0.0 && x>-1.5)  * (0.000265959)  \
-                                 + (x<-1.5)           * (-0.000136237)) \
+                                  ((x>0.0 && x<1.5)   * (0.95e-3)  \
+                                + (x>1.5)            * (0.07e-3)  \
+                               + (x<0.0 && x>-1.5)  * (0.26e-3)  \
+                                 + (x<-1.5)           * (-0.1e-5)) \
                                  + (y<0)*   \
-                                   ((x>0.0 && x<1.5)   * (-0.000184643) \
-                                 + (x>1.5)             * (0.000427552) \
-                                 + (x<0.0 && x>-1.5)  * (-0.000838635)  \
-                                 + (x<-1.5)           * (0.000408662))"),
+                                   ((x>0.0 && x<1.5)   * (-0.19e-3) \
+                                 + (x>1.5)             * (0.18e-3) \
+                                 + (x<0.0 && x>-1.5)  * (-0.85e-3)  \
+                                 + (x<-1.5)           * (0.19e-3))"),
+
+
+ # DetaBias = cms.untracked.string(" (x>0.0 && x<1.5)  * (-0.18e-3) \
+ #                                  +(x>1.5)           * (-0.31e-3)    \
+ #                                  +(x<0.0 && x>-1.5) * (0.18e-3)    \
+ #                                  +(x<-1.5)          * (0.31e-3) \
+ #                                  + y*0"),
+
+ # DphiBias = cms.untracked.string(" (y>0)*    \
+ #                                 ((x>0.0 && x<1.5)   * (0.605e-3)  \
+ #                                + (x>1.5)            * (0.035e-3)  \
+ #                                + (x<0.0 && x>-1.5)  * (0.605e-3)  \
+ #                                + (x<-1.5)           * (-0.035e-3)) \
+ #                                + (y<0)*   \
+ #                                  ((x>0.0 && x<1.5)   * (-0.52e-3) \
+ #                                + (x>1.5)             * (0.185e-3) \
+ #                                + (x<0.0 && x>-1.5)  * (-0.52e-3)  \
+ #                                + (x<-1.5)           * (0.185e-3))"),
+
   ## change input file appropriately
   inputFiles    = cms.vstring(
          # 'file:///eos/cms/store/group/dpg_ecal/alca_ecalcalib/twamorka/UltraLegacy_WithTrackerConditions_Conditions2/DoubleEG/crab_ZElectron-2017B-RAWReco-v1/190502_160024/0000/treeECALAlignment_499.root'
