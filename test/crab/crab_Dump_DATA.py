a9e8c027d73e0286d8b9c3bfc7ca97c278ec28ef
Amrutha Krishna <amruthakrishna.unni@gmail.com>
2022-10-10 13:26:00 +0200

    updated to use lumibased splitting

--- test/crab/crab_Dump_DATA.py
+++ test/crab/crab_Dump_DATA.py
@@ -4,34 +4,35 @@ config = Configuration()
 config.section_('General')
 config.General.transferOutputs = True
 #config.General.requestName = 'EcalAlignment_dump_DATA'
-config.General.requestName = 'EcalAlignment_RERECO_2022C_with_symmMCbias'
+config.General.requestName = 'EcalAlignment_RERECO_latestTrackerTag_fullEcal_v13_afterBugFix'
 
 config.section_('JobType')
 #config.JobType.psetName = '../RAW2RECO_DATA_cfg.py'
-config.JobType.psetName = '../rereco_cfg.py'
+config.JobType.psetName = '../rereco_cfg_newTrackerTag_forValidation.py'
 config.JobType.pluginName = 'Analysis'
 config.JobType.outputFiles = ['treeECALAlignment.root']
 config.JobType.pyCfgParams = ['inputFiles=FAKEINPUT', 'outputFile=treeECALAlignment.root']
 config.JobType.maxMemoryMB = 5000 
-config.JobType.inputFiles = ['EEAlign_run3_v4.db','EBAlign_run3_v0.db']
+config.JobType.inputFiles = ['EEAlign_run3_v12.db','EBAlign_run3_v13.db']
 #config.JobType.priority = 30
 #config.JobType.numCores = 2
 
 config.section_('Data')
 #config.Data.inputDataset = '/EGamma/Run2018A-12Nov2019_UL2018-v2/MINIAOD' 
 #config.Data.inputDataset = '/SingleElectron/Run2015B-PromptReco-v1/AOD'
-config.Data.inputDataset = '/EGamma/Run2022C-ZElectron-PromptReco-v1/RAW-RECO'
-config.Data.unitsPerJob = 3   # since files based, 3 files per job  
+config.Data.inputDataset = '/EGamma/Run2022D-ZElectron-PromptReco-v2/RAW-RECO'
+config.Data.unitsPerJob = 3   # since files based, 1 files per job  
 #config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
 #config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
-config.Data.splitting = 'FileBased'    # LumiBased
+config.Data.splitting = 'LumiBased'    # FileBased
 #config.Data.splitting = 'Automatic'
 #config.Data.useParent = True           #Important! is it?
-config.Data.runRange = '355862-356719'
+config.Data.runRange = '357734-357781'
+config.Data.lumiMask = '/eos/user/c/cmsdqm/www/CAF/certification/Collisions22/DCSOnly_JSONS/Cert_Collisions2022_355100_357900_13p6TeV_DCSOnly_TkPx.json'
 
 #config.Data.outLFN = '/store/user/amassiro/ECAL/Alignment/test16Mar2015'
 #config.Data.outLFN = '/store/group/dpg_ecal/alca_ecalcalib/amkrishn/ECAL_Alignment_test_Commissioning2021/' 
-config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/amkrishn/ECALalignment2022run3Commisioning/data/2022C'
+config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/amkrishn/ECALalignment2022run3Commisioning/data/2022D'
 
 config.section_('Site')
 config.Site.storageSite = 'T2_CH_CERN'
--- test/crab/crab_Dump_MC.py
+++ test/crab/crab_Dump_MC.py
@@ -3,7 +3,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 config.section_('General')
 config.General.transferOutputs = True
-config.General.requestName = 'MC_2018'
+config.General.requestName = 'crab_MC_2022_afterBugFix'
 
 config.section_('JobType')
 config.JobType.psetName = '../Dump_MC_cfg.py'
@@ -14,13 +14,16 @@ config.JobType.pyCfgParams = ['inputFiles=FAKEINPUT', 'outputFile=treeECALAlignm
 config.section_('Data')
 #config.Data.inputDataset = '/DYToEE_M-50_Tune4C_13TeV-pythia8/Phys14DR-PU40bx25_tsg_castor_PHYS14_25_V1-v2/AODSIM'
 #config.Data.inputDataset = '/DYToEE_M-50_Tune4C_13TeV-pythia8/Phys14DR-PU40bx25_tsg_castor_PHYS14_25_V1-v2/AODSIM'
-config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISpring18MiniAOD-NZSPU40to70_100X_upgrade2018_realistic_v10-v2/MINIAODSIM'
-config.Data.unitsPerJob = 10   # since files based, 10 files per job
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Winter22MiniAOD-122X_mcRun3_2021_realistic_v9_ext1-v1/MINIAODSIM'
+config.Data.unitsPerJob = 2   # since files based, 10 files per job
 config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
 #config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
 config.Data.splitting = 'FileBased'
 
 #config.Data.outLFN = '/store/user/amassiro/ECAL/Alignment/test16Jan2015'
-config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/twamorka/Alignment_2018/MC_2018'
+config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/amkrishn/ECALalignment2022run3Commisioning/MC/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/'
 config.section_('Site')
 config.Site.storageSite = 'T2_CH_CERN'
+
+
+
