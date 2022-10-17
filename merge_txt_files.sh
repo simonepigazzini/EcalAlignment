02bcd9d0bf2572ba62b60781ccf8479dcd08f4b2
amrutha-k <65673327+amrutha-k@users.noreply.github.com>
2022-10-17 15:47:43 +0200

    Update merge_txt_files.sh

--- merge_txt_files.sh
+++ merge_txt_files.sh
@@ -1,18 +1,18 @@
 #! /usr/bin/bash
 
-#merge EB alignment txt files
-touch EBAlignmentCoefficients_merged.txt 
+#merge EB alignment txt files 
 for i in {0..35}
 do
     cat EBAlignmentCoefficients_${i}.txt >> EBAlignmentCoefficients_merged.txt 
+    cat EBAlignmentCoefficients_${i}.txt.bis >> EBAlignmentCoefficients_with_err_merged.txt 
 done   
 echo "EB alignment coefficient txt files merged!"
 
 
 #merge EE alignment txt files
-touch EEAlignmentCoefficients_merged.txt
 for i in {0..3}
 do
-    cat EEAlignmentCoefficients_${i}.txt>> EEAlignmentCoefficients_merged.txt
+    cat EEAlignmentCoefficients_${i}.txt >> EEAlignmentCoefficients_merged.txt
+    cat EEAlignmentCoefficients_${i}.txt.bis >> EEAlignmentCoefficients_with_err_merged.txt
 done
 echo "EE alignment coefficient txt files merged!"
